e27c1cd9d208443ded91ac37aa0e262b71c658ff
dudaodong <lanliddd.2007@163.com>
2023-12-03 15:06:40 +0800

    feat: add symbol function

--- src/random/mod.rs
+++ src/random/mod.rs
@@ -1,16 +1,6 @@
 //! random mod contains several utility functions for generating random number or string.
 //!
 
-static NUMBERS: &str = "0123456789";
-static LOWER_LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
-static UPPER_LETTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-static LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-static ALPHANUMERIC: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-static SYMBOL_CHARS: &str = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
-
-static ALLCHARS: &str =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;':\",./<>?";
-
 mod numberic_str;
 pub use numberic_str::*;
 
@@ -26,5 +16,17 @@ pub use alphabeta::*;
 mod alpha_number;
 pub use alpha_number::*;
 
+mod symbol;
+pub use symbol::*;
+
 mod generate;
 use generate::*;
+
+static NUMBERS: &str = "0123456789";
+static LOWER_LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
+static UPPER_LETTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static ALPHANUMERIC: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static SYMBOL_CHARS: &str = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+static ALLCHARS: &str =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;':\",./<>?";
---
+++ src/random/symbol.rs
@@ -0,0 +1,48 @@
+use super::generate;
+
+/// Generate random string which only contains special chars
+/// (!@#$%^&*()_+-=[]{}|;':\",./<>?).
+///
+/// # Arguments
+///
+/// * `length` - The char size of random string.
+///
+/// # Returns
+///
+/// Returns random string with specific length.
+///
+/// # Examples
+///
+/// ```
+/// use ruf::random;
+///
+/// let symbol_str = random::symbol(6);
+/// assert_eq!(6, symbol_str.len());
+///
+/// let symbol_chars: &str = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+/// for c in symbol_str.chars() {
+///     assert!(symbol_chars.contains(c))
+/// }
+/// ```
+
+pub fn symbol(length: usize) -> String {
+    generate::generate_str("SYMBOL", length)
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_symbol() {
+        let symbol_str = symbol(6);
+
+        assert_eq!(6, symbol_str.len());
+
+        let symbol_chars: &str = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+        for c in symbol_str.chars() {
+            assert!(symbol_chars.contains(c))
+        }
+
+        assert_eq!("".to_string(), symbol(0));
+    }
+}
