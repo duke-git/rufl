d9e3604a08b09d1acbf4139cac8afd248885c2f6
dudaodong <lanliddd.2007@163.com>
2023-12-09 17:52:36 +0800

    refactor: add index param to predicate function

--- src/collection/filter.rs
+++ src/collection/filter.rs
@@ -1,5 +1,7 @@
 /// Iterates over elements of collection, returning a collection of all elements pass the predicate function.
 ///
+/// * predicate function signature: ```fn(item:T, index: usize) -> bool```
+///
 /// # Arguments
 ///
 /// * `collection` - The collection to iterate over.
@@ -15,17 +17,17 @@
 /// ```
 /// use ruf::collection;
 ///
-/// assert_eq!([1, 2, 3].to_vec(),collection::filter([1, 2, 3, 4, 5].to_vec(), &|n: i32| n <= 3));
+/// assert_eq!([1, 2, 3].to_vec(),collection::filter([1, 2, 3, 4, 5].to_vec(), &|n: i32, _i: usize| n <= 3));
 ///
-/// assert_eq!([2, 4].to_vec(),collection::filter([1, 2, 3, 4, 5].to_vec(), &|n: i32| n % 2 == 0));
+/// assert_eq!([2, 4].to_vec(),collection::filter([1, 2, 3, 4, 5].to_vec(), &|n: i32, _i: usize| n % 2 == 0));
 /// ```
 
-pub fn filter<T: Copy>(collection: Vec<T>, predicate: &dyn Fn(T) -> bool) -> Vec<T> {
+pub fn filter<T: Copy>(collection: Vec<T>, predicate: &dyn Fn(T, usize) -> bool) -> Vec<T> {
     let mut result: Vec<T> = Vec::new();
 
     for i in 0..collection.len() {
         let val = collection[i];
-        if predicate(val) {
+        if predicate(val, i) {
             result.push(val);
         }
     }
@@ -40,12 +42,12 @@ mod tests {
     fn test_filter() {
         assert_eq!(
             [1, 2, 3].to_vec(),
-            filter([1, 2, 3, 4, 5].to_vec(), &|n: i32| n <= 3)
+            filter([1, 2, 3, 4, 5].to_vec(), &|n: i32, _i: usize| n <= 3)
         );
 
         assert_eq!(
             [2, 4].to_vec(),
-            filter([1, 2, 3, 4, 5].to_vec(), &|n: i32| n % 2 == 0)
+            filter([1, 2, 3, 4, 5].to_vec(), &|n: i32, _i: usize| n % 2 == 0)
         );
     }
 }
