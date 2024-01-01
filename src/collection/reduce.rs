deea32e186d3f3563f0e5eccb31f08f75d88971b
dudaodong <lanliddd.2007@163.com>
2024-01-01 10:29:20 +0800

    refactor: refact reduce function

--- src/collection/reduce.rs
+++ src/collection/reduce.rs
@@ -1,15 +1,16 @@
 /// Reduces collection to a value which is the accumulated result of running each element in collection thru iteratee,
 /// where each successive invocation is supplied the return value of the previous.
 ///
-/// * iteratee function signature: ```fn(item: T, item: T) -> T```
+/// * accumulator function signature: ```fn(item: T, item: T, index: usize) -> T```
 ///
 /// # Arguments
 ///
 /// * `collection` - The collection to iterate over.
 ///
-/// * `accumulator` - The initial value.
+/// * `accumulator` - The function invoked per iteration.
 ///
-/// * `iteratee` - The function invoked per iteration.
+/// * `initial` - The initial value.
+///
 ///
 /// # Returns
 ///
@@ -20,32 +21,23 @@
 /// ```
 /// use ruf::collection;
 ///
-/// assert_eq!(15, collection::reduce(vec![1, 2, 3, 4, 5], &0, &|x: i32, y: i32| { x + y }));
+/// assert_eq!(15, collection::reduce(vec![1, 2, 3, 4, 5], &|x: i32, y: i32, i: usize| { x + y }, &0));
 ///
-/// assert_eq!(120, collection::reduce(vec![1, 2, 3, 4, 5], &1, &|x: i32, y: i32| { x * y }));
+/// assert_eq!(120, collection::reduce(vec![1, 2, 3, 4, 5], &|x: i32, y: i32, i: usize| { x * y }, &1));
 ///
 /// ```
 
 pub fn reduce<C: AsRef<[T]>, T: Copy>(
     collection: C,
-    accumulator: &T,
-    iteratee: &dyn Fn(T, T) -> T,
+    accumulator: &dyn Fn(T, T, usize) -> T,
+    initial: &T,
 ) -> T {
     let vector = collection.as_ref().to_vec();
 
-    if vector.len() == 0 {
-        return *accumulator;
-    }
+    let mut result = *initial;
 
-    let mut result = iteratee(*accumulator, vector[0]);
-
-    let mut reduce_arr: [T; 2] = [*accumulator, *accumulator];
-
-    for i in 1..vector.len() {
-        reduce_arr[0] = result;
-        reduce_arr[1] = vector[i];
-
-        result = iteratee(reduce_arr[0], reduce_arr[1]);
+    for i in 0..vector.len() {
+        result = accumulator(result, vector[i], i)
     }
 
     result
@@ -58,12 +50,20 @@ mod tests {
     fn test_filter() {
         assert_eq!(
             15,
-            reduce(vec![1, 2, 3, 4, 5], &0, &|x: i32, y: i32| { x + y })
+            reduce(
+                vec![1, 2, 3, 4, 5],
+                &|x: i32, y: i32, i: usize| { x + y },
+                &0
+            )
         );
 
         assert_eq!(
             120,
-            reduce(vec![1, 2, 3, 4, 5], &1, &|x: i32, y: i32| { x * y })
+            reduce(
+                vec![1, 2, 3, 4, 5],
+                &|x: i32, y: i32, i: usize| { x * y },
+                &1
+            )
         );
     }
 }
