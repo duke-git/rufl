813e1c7960fe64917b507000018a23075402af1b
dudaodong <lanliddd.2007@163.com>
2024-01-10 14:28:35 +0800

    feat: add is_ascending_order function

---
+++ src/collection/is_ascending_order.rs
@@ -0,0 +1,46 @@
+/// Checks if all elements are in ascending order within collection.
+///
+/// # Arguments
+///
+/// * `collection` - The collection to perform check.
+///
+/// # Returns
+///
+/// Returns true if all elements are in ascending order within collection.
+///
+/// # Examples
+///
+/// ```
+/// use ruf::collection;
+///
+/// assert_eq!(true, collection::is_ascending_order(&[1, 2, 3]));
+///
+/// assert_eq!(false, collection::is_ascending_order(&[3, 2, 1]));
+/// ```
+
+pub fn is_ascending_order<C: AsRef<[T]>, T: Ord>(collection: &C) -> bool {
+    let vec = collection.as_ref();
+
+    for i in 1..vec.len() {
+        let item1 = &vec[i - 1];
+        let item2 = &vec[i];
+        if item1 > item2 {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_ascending_order() {
+        assert_eq!(true, is_ascending_order(&[1]));
+        assert_eq!(true, is_ascending_order(&[1, 2, 3]));
+        assert_eq!(false, is_ascending_order(&[1, 3, 2]));
+        assert_eq!(false, is_ascending_order(&[3, 2, 1]));
+    }
+}
