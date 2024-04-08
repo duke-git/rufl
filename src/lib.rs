bc3839ff2714d1cc37513a4746313ee54c5557bd
dudaodong <lanliddd.2007@163.com>
2024-04-08 19:22:15 +0800

    feat: add feature tag

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
 //! Ruf is an util function library for rust.
 //! It provides a series of useful functions to make your rust development easier.
 //!
+#[cfg(feature = "collection")]
 pub mod collection;
+#[cfg(feature = "eventbus")]
 pub mod eventbus;
+
+#[cfg(feature = "math")]
 pub mod math;
+
+#[cfg(feature = "random")]
 pub mod random;
+
+#[cfg(feature = "string")]
 pub mod string;
